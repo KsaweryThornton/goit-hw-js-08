1c5933dcfb0ede4bbfe6ee714e7d769e1d49f26f
KsaweryThornton <ksawery.feliks.thornton@gmail.com>
2023-04-06 22:39:16 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
-//
+
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
